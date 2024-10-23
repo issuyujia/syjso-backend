1b647e88e0af0812284359edeeb1af06ff1f6031
suyujia <1747766732@qq.com>
2024-10-23 20:56:09 +0800

    初始化提交

---
+++ src/main/java/com/syj/syjso/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.syj.syjso.mapper;
+
+import com.syj.syjso.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/lisyj">程序员鱼皮</a>
+ * @from <a href="https://syj.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
